ee0c326a4895f3deb1cfa5aee6f239115d3ab652
Mauricio Neira <mauro@up.codes>
2021-12-10 23:07:26 -0800

    working gol

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::io::{stdin, stdout, Read, Write};
+use std::time::{SystemTime, UNIX_EPOCH};
 use termion::raw::IntoRawMode;
 use termion::{clear, color, cursor, style};
+
+use rand::Rng;
 
 const GAME_TITLE: &str = "Conway's game of life.";
 const GAME_AUTHOR: &str = "by Sharp Rabbit";
 const INSTRUCTIONS: &str = "q to quit, s to start, r to reset";
 const SQUARE_CHAR: &str = "#";
+const TIME_BETWEEN_FRAMES_IN_MILLISECONDS: u64 = 200;
 
 struct Game {
     width: u16,
@@ -41,9 +45,6 @@ impl Game {
 
     fn paint_state(&self) -> String {
         let mut output_string: String = format!("{}", clear::All,);
-        // for i in 1..10 {
-        //     output_string.push_str(&format!("{}{}", cursor::Goto(i, 5), SQUARE_CHAR)[..]);
-        // }
         for (row_i, row) in self.state.iter().enumerate() {
             for (col_i, value) in row.iter().enumerate() {
                 if *value {
@@ -55,17 +56,76 @@ impl Game {
                         )[..],
                     );
                 }
-
-                // output_string
-                //     .push_str(&format!("{}{}", cursor::Goto(3 as u16, 3 as u16), SQUARE_CHAR)[..]);
             }
         }
 
-        // println!("aaaa{}", output_string);
-
         return output_string;
     }
-    // fn evolve(&self) {}
+
+    fn get_neighbours(&self, row_i: usize, col_i: usize) -> usize {
+        let steps: Vec<i32> = vec![-1, 0, 1];
+
+        let mut neighbours = 0;
+
+        for row_step in steps.iter() {
+            for col_step in steps.iter() {
+                let row_window = ((row_i as i32 + row_step) % (self.height as i32)
+                    + self.height as i32)
+                    % self.height as i32;
+                let col_window = ((col_i as i32 + col_step) % (self.width as i32)
+                    + self.width as i32)
+                    % self.width as i32;
+
+                if self.state[row_window as usize][col_window as usize]
+                    && *row_step != 0
+                    && *col_step != 0
+                {
+                    neighbours += 1;
+                }
+            }
+        }
+
+        return neighbours;
+    }
+
+    fn evolve(&mut self) {
+        let mut new_state: Vec<Vec<bool>> = Vec::new();
+
+        for (row_i, row) in self.state.iter().enumerate() {
+            let mut new_row: Vec<bool> = Vec::new();
+
+            for (col_i, current_cell) in row.iter().enumerate() {
+                let neighbours = self.get_neighbours(row_i, col_i);
+
+                let mut new_cell = false;
+                if *current_cell && (neighbours == 2 || neighbours == 3) {
+                    new_cell = true;
+                }
+
+                if !current_cell && neighbours == 3 {
+                    new_cell = true;
+                }
+
+                new_row.push(new_cell);
+            }
+
+            new_state.push(new_row);
+        }
+
+        self.state = new_state;
+    }
+}
+
+fn get_time_now() -> u64 {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+
+    let in_ms =
+        since_the_epoch.as_secs() * 1000 + since_the_epoch.subsec_nanos() as u64 / 1_000_000;
+
+    return in_ms;
 }
 
 fn main() {
@@ -75,19 +135,30 @@ fn main() {
     let stdin = stdin();
     let stdin = stdin.lock();
 
+    let mut start_time = get_time_now();
+    let mut playing = false;
+
+    let mut rng = rand::thread_rng();
+
     let (width, height) = termion::terminal_size().ok().unwrap();
 
     let mut game_life = Vec::new();
 
-    for _ in 1..height {
+    for _ in 0..height {
         let mut temp_vec = Vec::new();
-        for _ in 1..width {
-            temp_vec.push(true);
+        for _ in 0..width {
+            let rand_number = rng.gen::<f64>();
+
+            if rand_number < 0.44 {
+                temp_vec.push(true);
+            } else {
+                temp_vec.push(false);
+            }
         }
         game_life.push(temp_vec);
     }
 
-    let game = Game {
+    let mut game = Game {
         width: width,
         height: height,
         state: game_life,
@@ -99,18 +170,36 @@ fn main() {
     let mut bytes = stdin.bytes();
     loop {
         let b = bytes.next().unwrap().unwrap();
-
+        println!("loooooooped");
         match b {
             // Quit
             b'q' => return,
             // Clear the screen
             b'c' => write!(stdout, "{}", termion::clear::All),
-            b's' => write!(stdout, "{}", game.paint_state()),
+            b's' => {
+                playing = true;
+                start_time = get_time_now();
+                write!(stdout, "{}", game.paint_state())
+            }
             b'a' => write!(stdout, "{}", game.get_main_screen()),
             // Write it to stdout.
             a => write!(stdout, "{}", a),
         }
         .unwrap();
+
+        // TODO: need to read from stdin in another thread to unblock this thread
+        // https://stackoverflow.com/a/55201400/10296312
+        if playing {
+            loop {
+                if get_time_now() - start_time > TIME_BETWEEN_FRAMES_IN_MILLISECONDS {
+                    game.evolve();
+                    write!(stdout, "{}", game.paint_state()).unwrap();
+                    start_time = get_time_now();
+
+                    stdout.flush().unwrap();
+                }
+            }
+        }
 
         stdout.flush().unwrap();
     }
